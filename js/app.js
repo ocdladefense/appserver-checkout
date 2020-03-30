276bb3c844623417749f3b343b50cea9fbf9963f
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2020-03-30 14:33:29 -0700

    ititial files

---
+++ js/app.js
@@ -0,0 +1,52 @@
+var mockCustomer = {
+		
+    FirstName: 'Jose',
+    
+    LastName: 'Bernal',
+    
+    Email: 'jbernal.web.dev@gmail.com',
+    
+    EmailRender: function(){
+        return 'email.render@gmail.com';
+    },
+
+    render:function(prop){
+        var rMethod = prop+'Render';
+        return this[rMethod] ? this[rMethod]() : this[prop];
+    },
+    
+};
+
+
+function showModal(){
+    // do a fetch of the template
+    
+    // convert the template to a string using the XML.serialize() method
+    var form = fetch('https://../html/cc_form.html');
+    form.then(response => response.text())
+    .then((text) => {
+        var xml = (new window.DOMParser()).parseFromString(text, "text/html"); //"text/xml"
+        console.log(xml);
+        document.getElementById('modal-content').innerHTML = parse(text,mockCustomer);
+    })
+    .then(()=>{
+    
+        document.body.setAttribute('class','hasModal');
+    
+        document.getElementById('cc-purchase-form').addEventListener('submit',function(e){return false;},false);
+        document.getElementById('cancel').addEventListener('click',function(e){return cancel();},false);
+        document.getElementById('purchase').addEventListener('click',function(e){return purchase();},false);
+    });
+}
+
+
+
+function purchase(){
+    alert('OK, will charge your card now.');
+        document.getElementById('modal-content').innerHTML = "<h3>Here's the document you requested:</h3><p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from &quote;de Finibus Bonorum et Malorum&quote; by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>";			
+}
+
+function cancel(){
+    document.body.setAttribute('class','');
+        document.getElementById('modal-content').innerHTML = "You don't have access to this resource.";
+}
---
+++ module.php
@@ -0,0 +1,21 @@
+<?php
+
+//route checkout renders this html
+
+
+
+	// 	<div id="page">
+		
+    //     <h1>CC Form Example</h1>
+    
+    //     <p>Access to this resource requires a paid subscription.</p>
+    
+    //     <button onclick="showModal();return false;">Purchase subscription - $149.99</button>
+    //     <div class="stage">
+
+    //         <div id="modal">
+    //             <div id="modal-content">&nbsp;</div>
+    //         </div>
+        
+    //     </div>
+    // </div>
